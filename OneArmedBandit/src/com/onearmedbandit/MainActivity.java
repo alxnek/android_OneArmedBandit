5355fcc7c70317a3ba6a478b423eaf1f93f4fc50
Alejandro Jorge <alxnek@gmail.com>
2013-02-24 16:00:19 +0100

    commit test

--- OneArmedBandit/src/com/onearmedbandit/MainActivity.java
+++ OneArmedBandit/src/com/onearmedbandit/MainActivity.java
@@ -1,15 +1,19 @@
 package com.onearmedbandit;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private final static String TAG = "MainActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.v(TAG, "Create layout, commit test");
 	}
 
 	@Override
