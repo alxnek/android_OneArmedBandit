82ac9ea55a3ff15bb7659a8c958d46c3824c207a
Alejandro Jorge <alxnek@gmail.com>
2013-03-11 18:59:45 +0100

    updating gitignore and buttons
    Conflicts:
    	OneArmedBandit/.classpath
    	OneArmedBandit/.settings/org.eclipse.jdt.core.prefs
    	OneArmedBandit/res/layout/activity_main.xml
    	OneArmedBandit/res/values/strings.xml
    	OneArmedBandit/src/com/onearmedbandit/MainActivity.java

--- OneArmedBandit/src/com/onearmedbandit/MainActivity.java
+++ OneArmedBandit/src/com/onearmedbandit/MainActivity.java
@@ -1,6 +1,5 @@
 package com.onearmedbandit;
 
-
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
@@ -17,9 +16,9 @@ public class MainActivity extends Activity implements OnClickListener
 
 	private final static String TAG = "MainActivity";
 
-	AnimationDrawable fruitAnimation1,fruitAnimation2,fruitAnimation3;
-	ImageView fruitView1,fruitView2,fruitView3;
-	Button b1,b2,b3,bStart;
+	AnimationDrawable fruitAnimation1, fruitAnimation2, fruitAnimation3;
+	ImageView fruitView1, fruitView2, fruitView3;
+	Button b1, b2, b3, bStart;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -27,25 +26,31 @@ public class MainActivity extends Activity implements OnClickListener
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
 		Log.v(TAG, "Create layout");
-		
+
 		this.bStart = (Button) this.findViewById(R.id.buttonStart);
-		bStart.setOnClickListener(this);
-		
+		this.bStart.setOnClickListener(this);
+
+		this.b1 = (Button) this.findViewById(R.id.button1);
+		this.b1.setOnClickListener(this);
+		this.b2 = (Button) this.findViewById(R.id.button2);
+		this.b2.setOnClickListener(this);
+		this.b3 = (Button) this.findViewById(R.id.button3);
+		this.b3.setOnClickListener(this);
+
 		this.fruitView1 = (ImageView) this.findViewById(R.id.ImageView01);
 		this.fruitView1.setBackgroundResource(R.anim.fruit);
 		this.fruitAnimation1 = (AnimationDrawable) this.fruitView1.getBackground();
-		
+
 		this.fruitView2 = (ImageView) this.findViewById(R.id.ImageView02);
 		this.fruitView2.setBackgroundResource(R.anim.fruit2);
 		this.fruitAnimation2 = (AnimationDrawable) this.fruitView2.getBackground();
-		
+
 		this.fruitView3 = (ImageView) this.findViewById(R.id.ImageView03);
 		this.fruitView3.setBackgroundResource(R.anim.fruit3);
 		this.fruitAnimation3 = (AnimationDrawable) this.fruitView3.getBackground();
-		
+
 	}
 
-	
 	/*
 	 * (Detect user touching the screen)
 	 * @see android.app.Activity#onTouchEvent(android.view.MotionEvent)
@@ -54,7 +59,7 @@ public class MainActivity extends Activity implements OnClickListener
 	public boolean onTouchEvent(MotionEvent event)
 	{
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
-		
+
 			Log.v(TAG, "TOUCH detected");
 			return true;
 		}
@@ -69,20 +74,32 @@ public class MainActivity extends Activity implements OnClickListener
 		return true;
 	}
 
-
 	@Override
 	public void onClick(View v)
 	{
-		if (v == bStart){
-			
+		if (v == this.bStart) {
+
 			this.fruitAnimation1.start();
 			this.fruitAnimation2.start();
 			this.fruitAnimation3.start();
 		}
+
+		if (v == this.b1) {
+
+			this.fruitAnimation1.stop();
+		}
+		if (v == this.b2) {
+
+			this.fruitAnimation2.stop();
+		}
+		if (v == this.b3) {
+
+			this.fruitAnimation3.stop();
+		}
 		//
-		
+
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
