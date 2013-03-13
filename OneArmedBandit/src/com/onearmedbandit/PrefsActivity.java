65ea7df6c945c522c633e03097bde504fa95b80f
Alejandro Jorge <alxnek@gmail.com>
2013-03-13 11:20:39 +0100

    Working with prefs

---
+++ OneArmedBandit/src/com/onearmedbandit/PrefsActivity.java
@@ -0,0 +1,15 @@
+package com.onearmedbandit;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PrefsActivity extends PreferenceActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.addPreferencesFromResource(R.xml.prefs);
+	}
+}
